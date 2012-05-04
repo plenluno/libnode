bc03499919dda9a7cb02cd5e13429f7a55a94529
Plenluno <plenluno@zoho.com>
2012-05-04 12:36:01 +0900

    add GTestUrlParser

---
+++ gtest/gtest_main.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ gtest/gtest_url_parser.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include <gtest/gtest.h>
+#include <url_parser.h>
+#include <libj/string.h>
+
+namespace libj {
+
+TEST(GTestUrlParser, TestParseUrl) {
+    struct parsed_url* url = parse_url("http://www.gtest.com:8888/foo/bar?abc=123&pqr=xyz");
+    ASSERT_EQ(String::create(url->scheme)->compareTo(String::create("http")), 0);
+    ASSERT_EQ(String::create(url->host)->compareTo(String::create("www.gtest.com")), 0);
+    ASSERT_EQ(String::create(url->port)->compareTo(String::create("8888")), 0);
+    ASSERT_EQ(String::create(url->path)->compareTo(String::create("foo/bar")), 0);
+    ASSERT_EQ(String::create(url->query)->compareTo(String::create("abc=123&pqr=xyz")), 0);
+    parsed_url_free(url);
+}
+
+}
