9eaf8750fb5496e7e508d2f8919f03d09020f824
Plenluno <plenluno@zoho.com>
2012-05-07 00:24:34 +0900

    handle request body

--- sample/echo_server.cpp
+++ sample/echo_server.cpp
@@ -7,14 +7,56 @@
 namespace libj {
 namespace node {
 
-class RequestHandler : LIBNODE_JS_FUNCTION(RequestHandler)
+class OnData : LIBNODE_JS_FUNCTION(OnData)
+ private:
+    Type<String>::Cptr body_;
+    
+ public:
+    OnData() : body_(String::create()) {}
+    
+    Type<String>::Cptr getBody() const { return body_; }
+    
+    Value operator()(Type<JsArray>::Cptr args) {
+        Type<String>::Cptr chunk = toCptr<String>(args->get(0));
+        if (chunk)
+            body_ = body_->concat(chunk);
+        return 0;
+    }
+};
+
+class OnEnd : LIBNODE_JS_FUNCTION(OnEnd)
+ private:
+    Type<OnData>::Ptr onData_;
+    Type<http::ServerRequest>::Ptr req_;
+    Type<http::ServerResponse>::Ptr res_;
+    
+ public:
+    OnEnd(
+        Type<OnData>::Ptr onData,
+        Type<http::ServerRequest>::Ptr req,
+        Type<http::ServerResponse>::Ptr res)
+        : onData_(onData)
+        , req_(req)
+        , res_(res) {}
+    
+    Value operator()(Type<JsArray>::Cptr args) {
+        req_->put(String::create("body"), onData_->getBody());
+        res_->setHeader(String::create("Content-Type"), String::create("text/plain"));
+        res_->write(json::stringify(req_));
+        res_->end();
+        return 0;
+    }
+};
+
+class OnRequest : LIBNODE_JS_FUNCTION(OnRequest)
  public:
     Value operator()(Type<JsArray>::Cptr args) {
         Type<http::ServerRequest>::Ptr req = toPtr<http::ServerRequest>(args->get(0));
         Type<http::ServerResponse>::Ptr res = toPtr<http::ServerResponse>(args->get(1));
-        res->setHeader(String::create("Content-Type"), String::create("text/plain"));
-        res->write(json::stringify(req));
-        res->end();
+        Type<OnData>::Ptr onData(new OnData());
+        Type<OnEnd>::Ptr onEnd(new OnEnd(onData, req, res));
+        req->on(http::ServerRequest::EVENT_DATA, onData);
+        req->on(http::ServerRequest::EVENT_END, onEnd);
         return 0;
     }
 };
@@ -25,7 +67,7 @@ class RequestHandler : LIBNODE_JS_FUNCTION(RequestHandler)
 int main() {
     using namespace libj;
     using namespace node;
-    Type<JsFunction>::Ptr requestHandler(new RequestHandler());
+    Type<OnRequest>::Ptr requestHandler(new OnRequest());
     Type<http::Server>::Ptr server = http::Server::create(requestHandler);
     server->listen(10000);
     run();
