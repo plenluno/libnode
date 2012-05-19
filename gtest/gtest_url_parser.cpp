0024908747be55bb773583df28a26902c19334af
Plenluno <plenluno@zoho.com>
2012-05-20 00:52:39 +0900

    add tests to GTestUrlParser

--- gtest/gtest_url_parser.cpp
+++ gtest/gtest_url_parser.cpp
@@ -7,22 +7,26 @@
 namespace libj {
 
 TEST(GTestUrlParser, TestParseUrl1) {
-    struct parsed_url* url = parse_url("/foo/bar?abc=123&pqr=xyz");
+    struct parsed_url* url = parse_url("/foo/bar?abc=123&pqr=xyz#fr");
     ASSERT_FALSE(url->scheme);
     ASSERT_FALSE(url->host);
     ASSERT_FALSE(url->port);
     ASSERT_EQ(String::create(url->path)->compareTo(String::create("foo/bar")), 0);
     ASSERT_EQ(String::create(url->query)->compareTo(String::create("abc=123&pqr=xyz")), 0);
+    ASSERT_EQ(String::create(url->fragment)->compareTo(String::create("fr")), 0);
     parsed_url_free(url);
 }
 
 TEST(GTestUrlParser, TestParseUrl2) {
-    struct parsed_url* url = parse_url("http://www.gtest.com:8888/foo/bar?abc=123&pqr=xyz");
+    struct parsed_url* url = parse_url("http://user1:passwd2@www.gtest.com:8888/foo/bar?abc=123&pqr=xyz#fr");
     ASSERT_EQ(String::create(url->scheme)->compareTo(String::create("http")), 0);
     ASSERT_EQ(String::create(url->host)->compareTo(String::create("www.gtest.com")), 0);
     ASSERT_EQ(String::create(url->port)->compareTo(String::create("8888")), 0);
     ASSERT_EQ(String::create(url->path)->compareTo(String::create("foo/bar")), 0);
     ASSERT_EQ(String::create(url->query)->compareTo(String::create("abc=123&pqr=xyz")), 0);
+    ASSERT_EQ(String::create(url->username)->compareTo(String::create("user1")), 0);
+    ASSERT_EQ(String::create(url->password)->compareTo(String::create("passwd2")), 0);
+    ASSERT_EQ(String::create(url->fragment)->compareTo(String::create("fr")), 0);
     parsed_url_free(url);
 }
 
