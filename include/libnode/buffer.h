382b6b6f5c1005a91ddfe4c0b7ced8a111f3ac1c
Plenluno <plenluno@zoho.com>
2012-06-03 23:26:06 +0900

    introduce Buffer (not implemented)

---
+++ include/libnode/buffer.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBNODE_BUFFER_H_
+#define LIBNODE_BUFFER_H_
+
+#include <libj/js_object.h>
+
+namespace libj {
+namespace node {
+
+class Buffer : LIBJ_JS_OBJECT(Buffer)
+};
+
+}  // namespace node
+}  // namespace libj
+
+#endif  // LIBNODE_BUFFER_H_
---
+++ src/buffer.cpp
@@ -0,0 +1,3 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include "libnode/buffer.h"
