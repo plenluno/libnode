143b716ccd0c37f506d3d09987dc62c99e14c1d6
Plenluno <plenluno@zoho.com>
2012-07-14 23:33:57 +0900

    add events.h

---
+++ include/libnode/events.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBNODE_EVENTS_H_
+#define LIBNODE_EVENTS_H_
+
+#include "libnode/events/event_emitter.h"
+
+#endif  // LIBNODE_EVENTS_H_
