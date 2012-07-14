91e013f3f99516393a55a742546b969ba6e83b28
Plenluno <plenluno@zoho.com>
2012-07-14 23:34:31 +0900

    add net.h

---
+++ include/libnode/net.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBNODE_NET_H_
+#define LIBNODE_NET_H_
+
+#include "libnode/net/socket.h"
+#include "libnode/net/server.h"
+
+#endif  // LIBNODE_NET_H_
