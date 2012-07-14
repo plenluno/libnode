6557f32b10d5ea1693f3b64c291e5a6c03e0de48
Plenluno <plenluno@zoho.com>
2012-07-14 23:31:39 +0900

    add http.h

---
+++ include/libnode/http.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBNODE_HTTP_H_
+#define LIBNODE_HTTP_H_
+
+#include "libnode/http/server.h"
+#include "libnode/http/server_request.h"
+#include "libnode/http/server_response.h"
+#include "libnode/http/status.h"
+
+#endif  // LIBNODE_HTTP_H_
