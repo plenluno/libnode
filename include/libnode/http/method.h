5cb58465e4fb1912d322d0138b4da752e870387d
Plenluno <plenluno@zoho.com>
2012-11-17 18:25:54 +0900

    add http method symbols

---
+++ include/libnode/http/method.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#ifndef LIBNODE_HTTP_METHOD_H_
+#define LIBNODE_HTTP_METHOD_H_
+
+#include <libj/symbol.h>
+
+namespace libj {
+namespace node {
+namespace http {
+
+#define LIBNODE_HTTP_METHOD_MAP(GEN) \
+    GEN(METHOD_DELETE,  "DELETE") \
+    GEN(METHOD_GET,     "GET") \
+    GEN(METHOD_HEAD,    "HEAD") \
+    GEN(METHOD_POST,    "POST") \
+    GEN(METHOD_PUT,     "PUT") \
+    GEN(METHOD_CONNECT, "CONNECT") \
+    GEN(METHOD_OPTIONS, "OPTIONS") \
+    GEN(METHOD_TRACE,   "TRACE")
+
+#define LIBNODE_HTTP_METHOD_DECL_GEN(NAME, VAL) \
+    extern Symbol::CPtr NAME;
+
+LIBNODE_HTTP_METHOD_MAP(
+    LIBNODE_HTTP_METHOD_DECL_GEN)
+
+}  // namespace http
+}  // namespace node
+}  // namespace libj
+
+#endif  // LIBNODE_HTTP_METHOD_H_
---
+++ src/http/method.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include "libnode/http/method.h"
+
+namespace libj {
+namespace node {
+namespace http {
+
+#define LIBNODE_HTTP_METHOD_DEF_GEN(NAME, VAL) \
+    LIBJ_SYMBOL_DEF(NAME, VAL)
+
+LIBNODE_HTTP_METHOD_MAP(
+    LIBNODE_HTTP_METHOD_DEF_GEN)
+
+}  // namespace http
+}  // namespace node
+}  // namespace libj
