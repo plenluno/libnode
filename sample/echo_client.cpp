e8b2b2ce12d86e17f11d88f4a7d040d67b2cf948
Plenluno <plenluno@zoho.com>
2012-11-18 13:13:03 +0900

    add echo client

---
+++ sample/echo_client.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2012 Plenluno All rights reserved.
+
+#include <libj/console.h>
+#include <libj/json.h>
+#include <libj/status.h>
+#include <libnode/buffer.h>
+#include <libnode/http.h>
+#include <libnode/node.h>
+#include <libnode/timer.h>
+
+namespace libj {
+namespace node {
+namespace echo {
+
+}  // namespace echo
+}  // namespace node
+}  // namespace libj
+
+int main() {
+    namespace node = libj::node;
+    namespace http = libj::node::http;
+
+    http::get(
+        libj::String::create("http://127.0.0.1:10000/abc"),
+        libj::JsFunction::null());
+    node::run();
+    return 0;
+}
