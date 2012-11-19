1c5d92661de736fd2b341645de360e3c0aa072e4
Plenluno <plenluno@zoho.com>
2012-11-20 00:03:38 +0900

    introduce callback queuing to nextTick

--- src/process.cpp
+++ src/process.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) 2012 Plenluno All rights reserved.
 
+#include <libj/status.h>
+
 #include "libnode/process.h"
 #include "libnode/timer.h"
 
@@ -7,9 +9,33 @@ namespace libj {
 namespace node {
 namespace process {
 
+class NextTick : LIBJ_JS_FUNCTION(NextTick)
+ public:
+    NextTick() : queue_(JsArray::create()) {}
+
+    Value operator()(JsArray::Ptr args) {
+        Size len = queue_->length();
+        for (Size i = 0; i < len; i++) {
+            JsFunction::Ptr cb = queue_->getPtr<JsFunction>(i);
+            (*cb)();
+        }
+        queue_->clear();
+        return Status::OK;
+    }
+
+    void push(JsFunction::Ptr cb) {
+        queue_->push(cb);
+    }
+
+ private:
+    JsArray::Ptr queue_;
+};
+
 void nextTick(JsFunction::Ptr callback) {
-    // TODO(plenluno): make it more efficient
-    setTimeout(callback, 0);
+    // TODO(plenluno): implement without setTimeout
+    static NextTick::Ptr nt(new NextTick());
+    if (callback) nt->push(callback);
+    setTimeout(nt, 0);
 }
 
 }  // namespace process
