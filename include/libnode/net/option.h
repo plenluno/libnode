423d4fa6b412eb7c16cf7b4f369efb23dc5d1b8e
Plenluno <plenluno@zoho.com>
2013-04-28 00:39:15 +0900

    modify indentation

--- include/libnode/net/option.h
+++ include/libnode/net/option.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Plenluno All rights reserved.
+// Copyright (c) 2012-2013 Plenluno All rights reserved.
 
 #ifndef LIBNODE_NET_OPTION_H_
 #define LIBNODE_NET_OPTION_H_
@@ -19,8 +19,7 @@ namespace net {
 #define LIBNODE_NET_OPTION_DECL_GEN(NAME, VAL) \
     extern Symbol::CPtr NAME;
 
-LIBNODE_NET_OPTION_MAP(
-    LIBNODE_NET_OPTION_DECL_GEN)
+LIBNODE_NET_OPTION_MAP(LIBNODE_NET_OPTION_DECL_GEN)
 
 }  // namespace net
 }  // namespace node
