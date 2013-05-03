16d3432ce3af278670c33c9be79f4806c77dd77e
Plenluno <plenluno@zoho.com>
2013-05-03 13:46:32 +0900

    introduce libj::trace

--- gtest/gtest_main.cpp
+++ gtest/gtest_main.cpp
@@ -4,11 +4,16 @@
 #include <libnode/debug_print.h>
 #include <libnode/detail/http/agent.h>
 
+#include <libj/trace.h>
 #include <libj/detail/gc_base.h>
 
 #include "./gtest_common.h"
 
 int main(int argc, char** argv) {
+    LIBJ_DEBUG_TRACE_ON;
+    LIBJ_DEBUG_TRACE_INCLUDE("^libj::node");
+    LIBJ_DEBUG_TRACE_INCLUDE("^libj::(.*)::create");
+
     testing::InitGoogleTest(&argc, argv);
     int r = RUN_ALL_TESTS();
 
@@ -30,5 +35,7 @@ int main(int argc, char** argv) {
     LIBJ_DEBUG_PRINT(
         "remaining objects: %d",
         LIBJ_DEBUG_OBJECT_COUNT);
+
+    LIBJ_DEBUG_TRACE_OFF;
     return r;
 }
