caaaa43f9db763dbdd9babe99b6d4442c1098723
Plenluno <plenluno@zoho.com>
2013-06-08 19:22:04 +0900

    introduce LIBNODE_DEBUG_TRACE_ON and LIBNODE_DEBUG_TRACE_OFF

---
+++ include/libnode/trace.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2013 Plenluno All rights reserved.
+
+#ifndef LIBNODE_TRACE_H_
+#define LIBNODE_TRACE_H_
+
+#include <libj/trace.h>
+
+#define LIBNODE_DEBUG_TRACE_ON \
+    LIBJ_DEBUG_TRACE_ON; \
+    LIBJ_DEBUG_TRACE_INCLUDE("^libj::node"); \
+    LIBJ_DEBUG_TRACE_INCLUDE("^libj::(.*)::create");
+
+#define LIBNODE_DEBUG_TRACE_OFF \
+    LIBJ_DEBUG_TRACE_OFF;
+
+#endif  // LIBNODE_TRACE_H_
