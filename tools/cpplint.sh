41d8435843874e77c1fc8f24deb998aa50a03f89
Plenluno <plenluno@zoho.com>
2013-06-09 17:47:31 +0900

    update tools/cpplint.sh

--- tools/cpplint.sh
+++ tools/cpplint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tools/cpplint/cpplint.py --filter=-runtime/explicit,-runtime/threadsafe_fn,-readability/streams,-build/include_what_you_use --prefix=libnode `find include src gtest sample -type f`
+tools/cpplint/cpplint.py --filter=-runtime/explicit,-runtime/threadsafe_fn,-readability/streams,-build/include_what_you_use --prefix=libnode `find include src gtest example -type f`
