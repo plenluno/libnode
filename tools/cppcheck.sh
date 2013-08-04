74af2d2223241990b4e425bd2c63c173093c60b1
Plenluno <plenluno@zoho.com>
2013-08-04 22:00:45 +0900

    update cppcheck settings

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cppcheck --xml --enable=all -f -q --includes-file=tools/cppcheck/include --suppressions-list=tools/cppcheck/suppress -DLIBJ_USE_BDW_GC include src
+cppcheck --xml --enable=all -f -q --includes-file=tools/cppcheck/include --suppressions-list=tools/cppcheck/suppress -DLIBJ_USE_BDWGC -DLIBNODE_USE_BDWGC include src example exts
