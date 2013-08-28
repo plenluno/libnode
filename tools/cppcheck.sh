569773ccc28e74d11b4aadf9dc0c52525a28c67c
Plenluno <plenluno@zoho.com>
2013-08-28 21:41:12 +0900

    exclude example/deps

--- tools/cppcheck.sh
+++ tools/cppcheck.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cppcheck --xml --enable=all -f -q --includes-file=tools/cppcheck/include --suppressions-list=tools/cppcheck/suppress -DLIBJ_USE_BDWGC -DLIBNODE_USE_BDWGC include src example exts
+cppcheck --xml --enable=all -f -q --includes-file=tools/cppcheck/include --suppressions-list=tools/cppcheck/suppress -DLIBJ_USE_BDWGC -DLIBNODE_USE_BDWGC include src exts example/*.h example/*.cpp
