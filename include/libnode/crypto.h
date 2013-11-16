c84c1faf1606d4098d997a97f8b6ed9998461f90
Plenluno <plenluno@zoho.com>
2013-11-16 15:57:46 +0900

    move crypto/rsa to exts

--- include/libnode/crypto.h
+++ include/libnode/crypto.h
@@ -3,7 +3,6 @@
 #ifndef LIBNODE_CRYPTO_H_
 #define LIBNODE_CRYPTO_H_
 
-#include <libnode/crypto/rsa.h>
 #include <libnode/crypto/hash.h>
 #include <libnode/crypto/cipher.h>
 #include <libnode/crypto/decipher.h>
