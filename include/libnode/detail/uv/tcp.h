d5c85c7e17afca77d17f97390f7d8af438b30708
Plenluno <plenluno@zoho.com>
2014-12-31 22:32:41 +0900

    remove setLastError() and enable connect() to return uv_errno_t

--- include/libnode/detail/uv/tcp.h
+++ include/libnode/detail/uv/tcp.h
@@ -18,12 +18,10 @@ class Tcp : public Stream {
     }
 
     virtual Int listen(Int backlog) {
-        Int r = uv_listen(
+        return uv_listen(
             reinterpret_cast<uv_stream_t*>(&tcp_),
             backlog,
             onConnection);
-        if (r) setLastError();
-        return r;
     }
 
     JsObject::Ptr getSockName() {
@@ -34,7 +32,6 @@ class Tcp : public Stream {
                     reinterpret_cast<struct sockaddr*>(&addr),
                     &len);
         if (r) {
-            setLastError();
             return JsObject::null();
         } else {
             return addressToJs(reinterpret_cast<const sockaddr*>(&addr));
@@ -49,7 +46,6 @@ class Tcp : public Stream {
                     reinterpret_cast<struct sockaddr*>(&addr),
                     &len);
         if (r) {
-            setLastError();
             return JsObject::null();
         } else {
             return addressToJs(reinterpret_cast<const sockaddr*>(&addr));
@@ -57,66 +53,51 @@ class Tcp : public Stream {
     }
 
     Int setNoDelay(Boolean enable) {
-        Int r = uv_tcp_nodelay(&tcp_, enable ? 1 : 0);
-        if (r) setLastError();
-        return r;
+        return uv_tcp_nodelay(&tcp_, enable ? 1 : 0);
     }
 
     Int setKeepAlive(Boolean enable, UInt delay) {
-        Int r = uv_tcp_keepalive(
-            &tcp_, enable ? 1 : 0, delay);
-        if (r) setLastError();
-        return r;
+        return uv_tcp_keepalive(&tcp_, enable ? 1 : 0, delay);
     }
 
     Int bind(String::CPtr ip, Int port = 0) {
-        struct sockaddr_in address =
-            uv_ip4_addr(ip->toStdString().c_str(), port);
-        Int r = uv_tcp_bind(&tcp_, address);
-        if (r) setLastError();
-        return r;
+        struct sockaddr_in addr;
+        int err = uv_ip4_addr(ip->toStdString().c_str(), port, &addr);
+        if (err) return err;
+
+        return uv_tcp_bind(
+            &tcp_,
+            reinterpret_cast<const sockaddr*>(&addr),
+            0);
     }
 
     Int bind6(String::CPtr ip6, Int port = 0) {
-        struct sockaddr_in6 address =
-            uv_ip6_addr(ip6->toStdString().c_str(), port);
-        Int r = uv_tcp_bind6(&tcp_, address);
-        if (r) setLastError();
-        return r;
+        struct sockaddr_in6 addr;
+        int err = uv_ip6_addr(ip6->toStdString().c_str(), port, &addr);
+        if (err) return err;
+
+        return uv_tcp_bind(
+            &tcp_,
+            reinterpret_cast<const sockaddr*>(&addr),
+            0);
     }
 
-    Connect* connect(String::CPtr ip, Int port) {
+    Int connect(String::CPtr ip, Int port, JsFunction::Ptr onComplete) {
         assert(ip);
-        struct sockaddr_in address =
-            uv_ip4_addr(ip->toStdString().c_str(), port);
-        Connect* creq = new Connect();
-        Int r = uv_tcp_connect(&creq->req, &tcp_, address, afterConnect);
-        creq->dispatched();
+        struct sockaddr_in addr;
+        int err = uv_ip4_addr(ip->toStdString().c_str(), port, &addr);
+        if (err) return err;
 
-        if (r) {
-            setLastError();
-            delete creq;
-            return NULL;
-        } else {
-            return creq;
-        }
+        return conn(reinterpret_cast<const sockaddr*>(&addr), onComplete);
     }
 
-    Connect* connect6(String::CPtr ip6, Int port) {
+    Int connect6(String::CPtr ip6, Int port, JsFunction::Ptr onComplete) {
         assert(ip6);
-        struct sockaddr_in6 address =
-            uv_ip6_addr(ip6->toStdString().c_str(), port);
-        Connect* creq = new Connect();
-        Int r = uv_tcp_connect6(&creq->req, &tcp_, address, afterConnect);
-        creq->dispatched();
+        struct sockaddr_in6 addr;
+        int err = uv_ip6_addr(ip6->toStdString().c_str(), port, &addr);
+        if (err) return err;
 
-        if (r) {
-            setLastError();
-            delete creq;
-            return NULL;
-        } else {
-            return creq;
-        }
+        return conn(reinterpret_cast<const sockaddr*>(&addr), onComplete);
     }
 
  private:
@@ -125,7 +106,6 @@ class Tcp : public Stream {
         assert(self && self->stream_ == handle);
 
         if (status) {
-            setLastError();
             invoke(self->onConnection_);
             return;
         } else {
@@ -133,6 +113,22 @@ class Tcp : public Stream {
             if (uv_accept(handle, tcp->stream_)) return;
             invoke(self->onConnection_, tcp);
         }
+    }
+
+    Int conn(const sockaddr* addr, JsFunction::Ptr onComplete) {
+        Connect* creq = new Connect();
+        creq->onComplete = onComplete;
+        creq->dispatched();
+
+        int err = uv_tcp_connect(
+            &creq->req,
+            &tcp_,
+            addr,
+            afterConnect);
+        if (err) {
+            delete creq;
+        }
+        return err;
     }
 
  private:
