fa6f22ad21752f3bde97339b21dacff7abc3a638
Plenluno <plenluno@zoho.com>
2015-01-02 11:59:13 +0900

    pass the number of bytes read to the callback

--- include/libnode/detail/uv/stream_common.h
+++ include/libnode/detail/uv/stream_common.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2014 Plenluno All rights reserved.
+// Copyright (c) 2012-2015 Plenluno All rights reserved.
 
 #ifndef LIBNODE_DETAIL_UV_STREAM_COMMON_H_
 #define LIBNODE_DETAIL_UV_STREAM_COMMON_H_
@@ -14,36 +14,41 @@ namespace uv {
 inline void Stream::onReadCommon(
     uv_stream_t* handle,
     ssize_t nread,
-    uv_buf_t buf,
+    const uv_buf_t* buf,
     uv_handle_type pending) {
     Stream* stream = static_cast<Stream*>(handle->data);
     JsFunction::Ptr onRead = stream->onRead_;
 
     if (nread < 0)  {
-        if (buf.base != NULL) {
+        if (buf->base) {
             stream->buffer_ = Buffer::null();
         }
-        setLastError();
 
-        if (onRead) invoke(onRead, Buffer::null(), 0);
+        if (onRead) invoke(onRead, nread, Buffer::null(), NULL);
         return;
     }
 
-    assert(buf.base != NULL);
+    if (nread == 0) {
+        if (buf->base) {
+            stream->buffer_ = Buffer::null();
+        }
+        return;
+    }
+
+    assert(buf->base);
+    assert(static_cast<size_t>(nread) <= buf->len);
     stream->buffer_ = stream->buffer_->slice(0, nread);
 
-    if (nread == 0) return;
-    assert(static_cast<size_t>(nread) <= buf.len);
-
-    Stream* pendingObj = NULL;
-    if (pending == UV_TCP) {
-        pendingObj = new Tcp();
-    } else if (pending == UV_NAMED_PIPE) {
-        pendingObj = new Pipe();
-    } else {
-        assert(pending == UV_UNKNOWN_HANDLE);
-    }
-    if (onRead) invoke(onRead, stream->buffer_, pendingObj);
+    // TODO(plenluno): create pendingObj
+    // Stream* pendingObj = NULL;
+    // if (type == UV_TCP) {
+    //     pendingObj = new Tcp();
+    // } else if (type == UV_NAMED_PIPE) {
+    //     pendingObj = new Pipe();
+    // } else {
+    //     assert(pending == UV_UNKNOWN_HANDLE);
+    // }
+    if (onRead) invoke(onRead, nread, stream->buffer_, NULL);
 }
 
 }  // namespace uv
